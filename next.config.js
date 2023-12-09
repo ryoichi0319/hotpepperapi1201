292f414971c84743092c58ab422343b897516bd6
ryoichi0319 <losegroove31@gmail.com>
2023-12-09 22:50:46 +0900

    like7

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
     images:{
         domains:["imgfp.hotp.jp","webservice.recruit.co.jp",
                  "lh3.googleusercontent.com","res.cloudinary.com"]
+    },
+    crossOrigin:{
+        origin: "*",
+        credentials: true,
     }
 }
 
