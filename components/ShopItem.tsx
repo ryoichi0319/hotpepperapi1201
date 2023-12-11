8371c4391679cc045dc80d7125de59eabedf3751
ryoichi0319 <losegroove31@gmail.com>
2023-12-11 23:55:38 +0900

    memo2

--- components/ShopItem.tsx
+++ components/ShopItem.tsx
@@ -1,40 +1,41 @@
-import Image from "next/image";
-import Link from "next/link";
-import ShopLikeDetail from "./ShopLikeDetail";
-import { memo } from "react";
-
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import ShopLikeDetail from './ShopLikeDetail';
 
 interface ShopItemProps {
-  shop: any
+  shop: any;
   userId: string | undefined;
 }
 
-
-const ShopItem = ({  shop, userId }: ShopItemProps) => {
-  
-
+const ShopItem = React.memo(({ shop, userId }: ShopItemProps) => {
   return (
-    <div className=" shadow-md rounded-md  mx-2 ">
-
-      <ul className="list-none  mx-1">
-        <li
-          key={shop.id}
-          className="bg-white py-10 px-6 my-5 mx-5  "
-        >
+    <div className=" shadow-md rounded-md mx-2">
+      <div className=' relative top-5 left-3'>
+      <ShopLikeDetail
+            userId={userId}
+            like={shop.like}
+            hasPostLiked={shop.hasPostLiked}
+            postLikeId={shop.postLikeId}
+            id={shop.id}
+          />
+          </div>
+      <ul className="list-none mx-1">
+        <li key={shop.id} className="bg-white py-10 px-6 my-5 mx-5 ">
           <div>
             <Link href={shop.urls.pc}>
               <div className="aspect-[16/9] relative overflow-hidden rounded-md">
-              <Image
-                className="   transition-all hover:scale-105 object-cover "
-                src={shop.photo.pc.m}
-                alt={shop.name}
-                width={250}
-                height={250}
-                priority
-              />
+                <Image
+                  className="transition-all hover:scale-105 object-cover"
+                  src={shop.photo.pc.m}
+                  alt={shop.name}
+                  width={250}
+                  height={250}
+                  priority
+                />
               </div>
             </Link>
-            </div>
+          </div>
 
           <div className="mt-2">
             <h3 className="text-lg font-semibold mb-2 mt-3">
@@ -44,17 +45,15 @@ const ShopItem = ({  shop, userId }: ShopItemProps) => {
             <p className="text-gray-700">{shop.address}</p>
             <p className="text-gray-700 font-bold">営業時間: </p>
             <p className=" text-gray-600">{shop.open}</p>
-            {/* <p className=" text-gray-700">バンド</p>
-            <p className=" text-gray-600">{shop.band}</p> */}
           </div>
-          <ShopLikeDetail  shop={shop} userId={userId}  />
-
+         
         </li>
       </ul>
-
+      
     </div>
-    
   );
-};
+});
 
 export default ShopItem;
+
+ShopItem.displayName = "ShopItem"
--- components/ShopLikeDetail.tsx
+++ components/ShopLikeDetail.tsx
@@ -1,25 +1,27 @@
 "use client"
 import { HeartIcon, HeartFilledIcon } from "@radix-ui/react-icons"
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { useRouter } from "next/navigation"
 import { trpc } from "@/trpc/react"
-
 
 import { cn } from "@/lib/utils"
 
   
   
   interface ShopLikeDetailProps {
-    shop: any 
     userId: string | undefined
+    like: any
+    hasPostLiked: boolean
+    postLikeId: string | undefined
+    id: string | undefined
         
   }
 
- const ShopLikeDetail =  ({shop,  userId}: ShopLikeDetailProps) => {
-    const filterLike = shop.like.filter((like:any) => like.postId === shop.id)
+ const ShopLikeDetail =  ({like,hasPostLiked,postLikeId,id,  userId}: ShopLikeDetailProps) => {
+    const filterLike = like.filter((like:any) => like.postId === id)
     const router = useRouter()
-    const [hasPostLiked, setHasPostLiked] = useState<boolean>(shop.hasPostLiked)
-    const [likePostCount, setLikePostCount] = useState<number>(filterLike.length)
+    const [has, setHasPostLiked] = useState<boolean>(hasPostLiked)
+    const [likePostCount, setLikePostCount] = useState<number>(like.length)
     
       //いいね追加
       const { mutate: createPostLike, isLoading: createPostLikeLoading} =
@@ -55,7 +57,7 @@ import { cn } from "@/lib/utils"
        
        
        const handleCreatePostLike = () => {
-        if (!shop.id) {
+        if (!id) {
             return;
         }
           
@@ -63,12 +65,12 @@ import { cn } from "@/lib/utils"
           setLikePostCount(likePostCount + 1)
   
           createPostLike({
-              postId: shop.id,
+              postId: id,
           })
        }
        const handleDeletePostLike = () =>{
 
-    if (!shop.id || !shop.postLikeId) {
+    if (!id || !postLikeId) {
               return
           }
           if(likePostCount > 0){
@@ -77,13 +79,10 @@ import { cn } from "@/lib/utils"
           }
   
           deletePostLike({
-              postLikeId: shop.postLikeId,
+              postLikeId: postLikeId,
               
           })
        }
-    useEffect(() =>{
-
-    },[])
 
       
 
@@ -91,7 +90,7 @@ import { cn } from "@/lib/utils"
        return(
         <div className=" flex items-center">
 
-            {shop.hasPostLiked ? (
+            {has ? (
                 
                 <button
                    className="hover:bg-gray-100 p-2 rounded-full"
