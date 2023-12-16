d37a433955f60476725c72e97e9c3e7d5cdca1ea
ryoichi0319 <losegroove31@gmail.com>
2023-12-16 22:48:34 +0900

    test2

--- components/author/AuthorDetail.tsx
+++ components/author/AuthorDetail.tsx
@@ -47,7 +47,7 @@ const AuthorDetail = ({user,likes}: AuthorDetailProps) => {
                         ))}
 
                     </div>
-                )}
+                )}  
             </div>
 
         </div>
